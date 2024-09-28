3b7bbcc1f1bc97a29c540a46937db0c64e904275
Paulo Magno Rodrigues Salum <paulo.mrsalum@gmail.com>
2024-09-28 19:28:35 -0300

    Update update_readme.py

--- update_readme.py
+++ update_readme.py
@@ -6,7 +6,7 @@ current_year = datetime.datetime.now().year
 years_experience = current_year - 2018
 
 # Read the README.md file
-with open('README.md', 'r', encoding='utf-8') as file:
+with open('Readme.md', 'r', encoding='utf-8') as file:
     content = file.read()
 
 # Replace the placeholder with the calculated years
